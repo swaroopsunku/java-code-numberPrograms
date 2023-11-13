8cb5ea0b5dc54647599f8d375ba11cf00c1ba4a9
chandra swaroop <chandraswaroop6630@gmail.com>
2023-11-13 19:47:27 +0530

    swap number

---
+++ Swap.java
@@ -0,0 +1,12 @@
+package standard_programs;
+
+public class Swap {
+	public static void main(String[] args){
+	    int a=20,b=100;
+	    a=a+b;
+	    b=a-b;
+	    a=a-b;
+	    System.out.println("a value is "+a);
+	    System.out.println("b value is "+b);
+	}
+}
